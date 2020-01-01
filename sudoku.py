c81fe1dbfe09d185458e87f14581133d310a2c2f
AvinashJN <49609169+AvinashPython@users.noreply.github.com>
2020-01-01 18:17:38 -0500

    Completed Sudoku solver using pycharm

--- sudoku.py
+++ sudoku.py
@@ -13,7 +13,30 @@ sod_main = np.array([[0,2,0,0,7,0,1,9,0],
 
 def check_correct(num, row, col, mat):
     ''' Function to check if a number (num) is okay to be kept in row and col of the sudoku puzzle (mat)'''
-    if n in mat[i] or n in mat[:, j] or n in mat[(i // 3) * 3:(3 + (i // 3) * 3), (j // 3) * 3:(3 + (j // 3) * 3)]:
+    if num in mat[row] or num in mat[:, col] or num in mat[(row // 3) * 3:(3 + (row // 3) * 3), (col // 3) * 3:(3 + (col // 3) * 3)]:
         return False
     return True
 
+def sudoku_solve(row, col, mat):
+    if row==9:
+        return True
+    if col==9:
+        row = row + 1
+        col = 0
+        return sudoku_solve(row, col, mat)
+    if mat[row][col]!=0:
+        return sudoku_solve(row,col+1, mat)
+
+    for n in range(1,10):
+        if check_correct(n,row, col, mat):
+            mat[row][col]=n
+            if sudoku_solve(row, col+1, mat):
+                return True
+            mat[row][col]=0
+    return False
+
+sudoku_solve(0,0,sod_main)
+print(sod_main)
+
+for i in range(9):
+    print(np.sum(sod_main[i]), np.sum(sod_main[:,i]))
